d91bfe46023a3b184dfecfbe3924acb645618748
杜培东 <dupeidong@youplus.cc>
2017-03-04 14:32:55 +0800

    dist

--- admin/middleware/auth.js
+++ admin/middleware/auth.js
@@ -1,14 +1,15 @@
 /**
 * 用于验证用户是否登录
 */
-module.exports = class AuthClient {
-    constructor() {
-        
-    }
-    loggedIn() {
-    	if(!store.get('token')){
-    		 return false;
-    	};
-    	return true;
-    }
+
+function Auth() {
+
 }
+Auth.prototype.loggedIn = function(){
+	if(!store.get('token')){
+		 return false;
+	};
+	return true;
+}
+
+export default Auth; 
