4d5140e2624c655b06f2a7a5306662e192c66670
杜培东 <dupeidong@youplus.cc>
2017-04-08 17:58:36 +0800

    修改上传样式

--- admin/src/components/edit.vue
+++ admin/src/components/edit.vue
@@ -29,16 +29,13 @@
                 </el-form-item>
                 <el-form-item label="上传缩略图">
                     <el-upload
-                        action="/api/upload"
-                        type="drag"
+                        action="/api/upload/"
+                        list-type="picture-card"
                         :headers="headers"
-                        :thumbnail-mode="true"
                         :on-remove="removeImage"
                         :on-success="successUpload"
-                        :default-file-list="fileList">
-                            <i class="el-icon-upload"></i>
-                            <div class="el-dragger__text">将文件拖到此处，或<em>点击上传</em></div>
-                            <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+                        :file-list="fileList">
+                            <i class="el-icon-plus"></i>
                     </el-upload>
                 </el-form-item>
                 <el-form-item>
