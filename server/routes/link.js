e206dc710af480ed87d0ee27c85f741f8a217482
杜培东 <dupeidong@youplus.cc>
2017-04-15 14:59:44 +0800

    seo

--- server/routes/link.js
+++ server/routes/link.js
@@ -26,6 +26,7 @@ router.get('/', function(req, res, next) {
 	  		{ 
 	  			title: '前端' ,
 	  			cate_id:-1,
+	  			pathUrl:'/home/2',
 	  			posts: o.collections,
 	  			count:o.count,
 	  			nodata:o.nodata,
@@ -34,6 +35,27 @@ router.get('/', function(req, res, next) {
 	  	);
 	})	
 	  	
+});
+router.get('/home/:page_id', function(req, res, next) {
+
+	var query = extendConditions({});
+	query.offset = req.params.page_id-1;
+	ArticleModel.getPagination(query,function(err,o){
+		if(o.collections.length < query.limit){
+			o.nodata = true; 
+		};
+		res.render('index', 
+	  		{ 
+	  			title: '前端',
+	  			pathUrl:'/home/'+ (req.params.page_id-0+1),
+	  			cate_id:-1,
+	  			posts: o.collections,
+	  			count:o.count,
+	  			nodata:o.nodata,
+	  			menus:dict.articleTypes
+	  		}
+	  	);
+	})	  	
 });
 router.get('/cate/:id', function(req, res, next) {
 	var query = extendConditions({post_classify_sup:req.params.id});
@@ -45,6 +67,28 @@ router.get('/cate/:id', function(req, res, next) {
 		res.render('index', 
 	  		{ 
 	  			title: '前端',
+	  			pathUrl:'/cate/'+req.params.id+'/2',
+	  			cate_id:req.params.id,
+	  			posts: o.collections,
+	  			count:o.count,
+	  			nodata:o.nodata,
+	  			menus:dict.articleTypes
+	  		}
+	  	);
+	})	  	
+});
+router.get('/cate/:id/:page_id', function(req, res, next) {
+
+	var query = extendConditions({post_classify_sup:req.params.id});
+	query.offset = req.params.page_id-1;
+	ArticleModel.getPagination(query,function(err,o){
+		if(o.collections.length < query.limit){
+			o.nodata = true; 
+		};
+		res.render('index', 
+	  		{ 
+	  			title: '前端',
+	  			pathUrl:'/cate/'+req.params.id+'/'+(req.params.page_id-0+1),
 	  			cate_id:req.params.id,
 	  			posts: o.collections,
 	  			count:o.count,
@@ -63,6 +107,27 @@ router.get('/subcate/:id', function(req, res, next) {
 		res.render('index', 
 	  		{ 
 	  			title: '前端' ,
+	  			cate_id:0,
+	  			posts: o.collections,
+	  			count:o.count,
+	  			nodata:o.nodata,
+	  			menus:dict.articleTypes
+	  		}
+	  	);
+	})	  	
+});
+router.get('/subcate/:id/:page_id', function(req, res, next) {
+
+	var query = extendConditions({post_classify_sub:req.params.id});
+	query.offset = req.params.page_id-1;
+	ArticleModel.getPagination(query,function(err,o){
+		if(o.collections.length < query.limit){
+			o.nodata = true; 
+		};
+		res.render('index', 
+	  		{ 
+	  			title: '前端',
+	  			pathUrl:'/subcate/'+req.params.id+'/'+(req.params.page_id-0+1),
 	  			cate_id:0,
 	  			posts: o.collections,
 	  			count:o.count,
